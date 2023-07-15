a689926aea0edb4add967965cd2b30ca85c4bf5c
Jason Savelli <jason.savelli@mention.com>
2023-07-15 11:06:56 +0200

    chore: setup tailwind with prettier

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+// prettier.config.js
+module.exports = {
+  tailwindFunctions: ["classnames"],
+  tailwindConfig: "./tailwind.config.js",
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
+ 
