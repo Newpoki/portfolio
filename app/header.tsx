aff75401952bc68d5e3e272ffdb02c5006025c78
Jason Savelli <jason.savelli@mention.com>
2023-07-15 09:12:11 +0200

    feat: create home page

---
+++ app/header.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <header className="flex px-6 py-4 justify-between">
+      <h1>Jason Savelli</h1>
+
+      <div className="flex gap-8">
+        <a>Home</a>
+        <a>Projects</a>
+        <a>Experiencies</a>
+        <a>About</a>
+      </div>
+    </header>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Jason Savelli - Home",
+  description: "The home page",
+};
 
 export default function Home() {
-  return <main>main</main>;
+  return (
+    <main>
+      <h1 className="flex flex-col mb-8">
+        <span className="text-9xl font-bold uppercase">Jason Savelli</span>
+        <span className="text-9xl font-bold uppercase">Front End developper</span>
+      </h1>
+
+      <h2 className="text-2xl font-semibold">I enjoy working on React projects with TypeScript</h2>
+    </main>
+  );
 }
