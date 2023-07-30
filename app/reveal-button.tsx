ae73abf5d84d1ad0406bc83b79c7808c57d81195
Jason Savelli <jason.savelli@mention.com>
2023-07-30 16:48:10 +0200

    feat: reveal button now responsive

--- app/reveal-button.tsx
+++ app/reveal-button.tsx
@@ -1,16 +1,35 @@
+import classNames from "classnames";
+
 type IRevealButton = {
   children: React.ReactNode;
   hidden: React.ReactNode;
+  size?: "small" | "normal";
 };
 
-export const RevealButton = ({ children, hidden }: IRevealButton) => {
+export const RevealButton = ({ children, hidden, size }: IRevealButton) => {
   return (
     <button
       type="button"
-      className="group relative items-center overflow-hidden rounded-3xl border-2 border-black px-8 py-3 font-semibold"
+      className={classNames(
+        "group relative items-center overflow-hidden rounded-3xl border-2 border-black font-semibold",
+        {
+          "px-8 py-3": size === "normal",
+          "px-5 py-2": size === "small",
+          "px-8 py-3 lg:px-5 lg:py-2": size == null,
+        }
+      )}
     >
       {children}
-      <span className="absolute left-0 right-0 top-0 flex h-full translate-y-12 items-center whitespace-nowrap bg-black px-8 py-3 text-white transition-transform group-hover:translate-y-0">
+      <span
+        className={classNames(
+          "absolute left-0 right-0 top-0 flex h-full translate-y-12 items-center whitespace-nowrap bg-black text-white transition-transform group-hover:translate-y-0",
+          {
+            "px-8 py-3": size === "normal",
+            "px-5 py-2": size === "small",
+            "px-8 py-3 lg:px-5 lg:py-2": size == null,
+          }
+        )}
+      >
         {hidden}
       </span>
     </button>
