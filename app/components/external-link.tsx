ec430aeb3a2b05ee4e3ba70b5fb508d27d3a3fd3
Jason Savelli <jason.savelli@mention.com>
2023-08-05 11:35:42 +0200

    feat: add external link component

---
+++ app/components/external-link.tsx
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+import { Typography } from "./typography";
+import { forwardRef } from "react";
+
+type Props = React.DetailedHTMLProps<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>;
+
+export const ExternalLink = forwardRef<HTMLAnchorElement, Props>(
+  ({ className, ...props }, ref) => {
+    return (
+      <Typography
+        {...props}
+        as="a"
+        className={classNames(
+          "after:content-[' '] :hover relative flex flex-col self-start before:absolute before:bottom-0 before:inline-block before:h-[1px] before:w-[10%] before:translate-x-[-100%] before:bg-white before:transition-transform before:duration-500 after:inline-block after:h-[1px] after:w-full after:bg-black hover:before:translate-x-[1100%]",
+          className
+        )}
+        ref={ref}
+      />
+    );
+  }
+);
