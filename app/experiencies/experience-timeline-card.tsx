22fedacade07b0ee2f38b4b11207f79b598d2589
Jason Savelli <jason.savelli@mention.com>
2023-08-06 11:57:51 +0200

    task: make timeline usable on mobile

--- app/experiencies/experience-timeline-card.tsx
+++ app/experiencies/experience-timeline-card.tsx
@@ -11,10 +11,10 @@ type Props = {
 
 export const ExperienceTimelineCard = ({ source, title }: Props) => {
   return (
-    <li className="before:content-[' '] flex before:flex-1 before:border-1  before:border-transparent before:p-10 [&:nth-child(2n)]:flex-row-reverse">
+    <li className="before:content-[' '] flex before:hidden before:flex-1 before:border-1 before:border-transparent before:p-10 xl:before:block xl:[&:nth-child(2n)]:flex-row-reverse">
       <ExperienceTimelineSeparator />
 
-      <div className="w-1/2 flex-1 rounded-md border-1 border-gray-600 p-10">
+      <div className="mb-6 w-1/2 flex-1 rounded-md border-1 border-gray-600 p-10">
         <Typography className="mb-4">{title}</Typography>
 
         <div className="prose">
--- app/experiencies/experience-timeline-separator.tsx
+++ app/experiencies/experience-timeline-separator.tsx
@@ -1,6 +1,6 @@
 export const ExperienceTimelineSeparator = () => {
   return (
-    <div className="flex flex-col items-center px-4">
+    <div className="flex flex-col items-center px-4 pl-0 xl:pl-4">
       <div className="flex w-0.5 flex-grow bg-black" />
       <div className="h-8 w-8 bg-red-500" />
       <div className="flex w-0.5 flex-grow bg-black" />
