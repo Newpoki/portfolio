9ee1af46bb80ecea2fa83bdd93d63688d88892d8
Jason Savelli <jason.savelli@mention.com>
2023-10-07 18:52:28 +0200

    task: max widtht on body to improve render on wide screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
+import classNames from "classnames";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,7 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={classNames(inter.className, "mx-auto max-w-[1996px]")}>
         <main className="flex h-[100dvh] flex-col px-4 pb-4 lg:px-12 xl:px-24">
           <Header />
 
