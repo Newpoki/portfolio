66e0a7b665035ecf8d02d4b67dc6a6b5666f7efb
Jason Savelli <jsavelli@splio.com>
2025-02-09 22:55:18 +0100

    feat: clicking on link close drawer

--- app/header/header-sheet.tsx
+++ app/header/header-sheet.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { MenuIcon } from "lucide-react";
 import { HeaderLink } from "./header-link";
 import { DialogTitle } from "@radix-ui/react-dialog";
+import { useCallback, useState } from "react";
 
 type HeaderDrawerProps = {
   className: string;
 };
 export const HeaderSheet = ({ className }: HeaderDrawerProps) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleOpenDrawer = useCallback(() => {
+    setIsDrawerOpen(true);
+  }, []);
+
+  const handleCloseDrawer = useCallback(() => {
+    setIsDrawerOpen(false);
+  }, []);
+
   return (
-    <Drawer>
+    <Drawer open={isDrawerOpen} onOpenChange={handleOpenDrawer}>
       <DrawerTrigger className={className}>
         <MenuIcon />
       </DrawerTrigger>
@@ -17,23 +30,39 @@ export const HeaderSheet = ({ className }: HeaderDrawerProps) => {
         <DialogTitle className="hidden">Mobile navigation menu</DialogTitle>
 
         <nav className="flex flex-1 flex-col justify-center gap-6 ">
-          <HeaderLink className="w-fit" href="/">
+          <HeaderLink onClick={handleCloseDrawer} className="w-fit" href="/">
             home
           </HeaderLink>
 
-          <HeaderLink className="w-fit" href="/projects">
+          <HeaderLink
+            onClick={handleCloseDrawer}
+            className="w-fit"
+            href="/projects"
+          >
             projects
           </HeaderLink>
 
-          <HeaderLink className="w-fit" href="/experiencies">
+          <HeaderLink
+            onClick={handleCloseDrawer}
+            className="w-fit"
+            href="/experiencies"
+          >
             experiencies
           </HeaderLink>
 
-          <HeaderLink className="w-fit" href="/about">
+          <HeaderLink
+            onClick={handleCloseDrawer}
+            className="w-fit"
+            href="/about"
+          >
             about
           </HeaderLink>
 
-          <HeaderLink className="w-fit" href="/references">
+          <HeaderLink
+            onClick={handleCloseDrawer}
+            className="w-fit"
+            href="/references"
+          >
             references
           </HeaderLink>
         </nav>
