aee3861e0bf4f436041e676de69ac134c02169a7
Jason Savelli <jsavelli@splio.com>
2025-02-09 22:01:53 +0100

    task: add shadcn

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -1,21 +1,9 @@
-import { HeaderLink } from "./header-link";
 import JavascriptIcon from "@/public/icons/javascript.svg";
-import { HeaderMenu } from "./header-menu";
 
 export const Header = () => {
   return (
-    <div className="sticky top-0 z-50 mb-8 flex items-center justify-between bg-white py-4 font-semibold">
-      <HeaderMenu />
-
+    <div className="sticky top-0 z-50 mb-8 flex items-center justify-between py-4">
       <JavascriptIcon className="w-8 lg:w-8" />
-
-      <nav className="hidden gap-8 lg:flex">
-        <HeaderLink href="/">home</HeaderLink>
-        <HeaderLink href="/projects">projects</HeaderLink>
-        <HeaderLink href="/experiencies">experiencies</HeaderLink>
-        <HeaderLink href="/about">about</HeaderLink>
-        <HeaderLink href="/references">references</HeaderLink>
-      </nav>
     </div>
   );
 };
