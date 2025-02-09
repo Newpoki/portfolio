ed5c71eaac810865b1b9aab49c327a183d95d7a7
Jason Savelli <jsavelli@splio.com>
2025-02-09 22:11:12 +0100

    feat: mobile header

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -1,8 +1,11 @@
 import JavascriptIcon from "@/public/icons/javascript.svg";
+import { MenuIcon } from "lucide-react";
 
 export const Header = () => {
   return (
-    <div className="sticky top-0 z-50 mb-8 flex items-center justify-between py-4">
+    <div className="main-layout-page-wrapper sticky top-0 z-50 mb-8 flex items-center justify-between border-b-1 border-b-gray-100 py-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <MenuIcon />
+
       <JavascriptIcon className="w-8 lg:w-8" />
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,10 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={classNames(inter.className, "mx-auto max-w-[1996px]")}>
-        <main className="flex min-h-[100dvh] flex-col px-4 pb-4 lg:px-12 xl:px-24">
+        <main className="flex min-h-[100dvh] flex-col pb-4">
           <Header />
 
-          {children}
-
+          <div className="main-layout-page-wrapper">{children}</div>
           <Footer />
         </main>
       </body>
