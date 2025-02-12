9fd893f48ad1ae6daa408e75fa5d71fef619d76e
Jason Savelli <jsavelli@splio.com>
2025-02-12 21:02:49 +0100

    task: update resume download button wording

--- app/(home)/download-resume-button.tsx
+++ app/(home)/download-resume-button.tsx
@@ -17,7 +17,7 @@ export const DownloadResumeButton = ({
         className,
       )}
     >
-      Download my CV
+      Download my resume
       <span className="bg-foreground text-background absolute top-0 right-0 left-0 flex h-full translate-y-12 items-center px-8 py-3 whitespace-nowrap transition-transform group-hover:translate-y-0 lg:px-5 lg:py-2">
         <div className="flex h-12 items-center gap-4">
           <DownloadIcon width={16} />
