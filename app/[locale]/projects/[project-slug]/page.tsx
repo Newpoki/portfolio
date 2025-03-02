34c248dedd9ccaa64fc58c0a288488e4216509fc
Jason Savelli <jsavelli@splio.com>
2025-03-02 22:06:35 +0100

    feat: project image is now always fully visible whatever the screen width is

--- app/[locale]/projects/[project-slug]/page.tsx
+++ app/[locale]/projects/[project-slug]/page.tsx
@@ -39,59 +39,61 @@ export default async function Project({ params }: Props) {
   const t = await getTranslations("PROJECT");
 
   return (
-    <div>
-      <h1 className="mb-8 flex items-center justify-between">
-        <span>{project.name}</span>
+    <div className="flex flex-col lg:gap-8">
+      <div className="flex max-h-[calc(100dvh-var(--header-h)-var(--header-m-b))] flex-col gap-8 pb-10">
+        <h1 className="flex items-center justify-between">
+          <span>{project.name}</span>
 
-        <span>
-          {t("deployed-at-content", {
-            deployedAt: project.deployedAt.getFullYear(),
-          })}
-        </span>
-      </h1>
-
-      <section className="relative flex flex-col lg:mb-8">
-        <div className="mb-12 flex flex-col gap-6 md:flex-row md:items-center md:justify-between">
-          <h2 className="xl:max-w-2xl">{project.shortDesc}</h2>
-
-          <span className="font-semibold whitespace-nowrap">
-            {t("discover")}
+          <span>
+            {t("deployed-at-content", {
+              deployedAt: project.deployedAt.getFullYear(),
+            })}
           </span>
-        </div>
+        </h1>
 
-        <Image
-          src={project.illustration}
-          alt={project.illustrationAlt}
-          fill
-          className="relative! object-cover"
-          priority
-        />
+        <section className="flex min-h-full flex-col gap-10 md:gap-28">
+          <div className="flex flex-col gap-6 md:flex-row md:items-center md:justify-between">
+            <h2 className="xl:max-w-4xl">{project.shortDesc}</h2>
 
-        <section className="mx-auto flex flex-col justify-center gap-16 md:w-4/5 lg:w-1/2">
-          <h3 className="text-xl">{project.description}</h3>
+            <span className="font-semibold whitespace-nowrap">
+              {t("discover")}
+            </span>
+          </div>
 
-          {project.websiteUrl && project.githubUrl && (
-            <div className="sm:space-between flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-              <Link
-                type="external"
-                href={project.websiteUrl}
-                animation="bright-slide"
-              >
-                {t("visit-live-version")}
-              </Link>
-
-              <Link
-                type="external"
-                href={project.githubUrl}
-                animation="bright-slide"
-              >
-                {t("visit-github")}
-              </Link>
-            </div>
-          )}
-
-          <ProjectInfosList project={project} />
+          <Image
+            src={project.illustration}
+            alt={project.illustrationAlt}
+            fill
+            className="relative! min-h-full object-contain"
+            priority
+          />
         </section>
+      </div>
+
+      <section className="mx-auto flex flex-col justify-center gap-16 md:w-4/5 lg:w-1/2">
+        <h3 className="text-xl">{project.description}</h3>
+
+        {project.websiteUrl && project.githubUrl && (
+          <div className="sm:space-between flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+            <Link
+              type="external"
+              href={project.websiteUrl}
+              animation="bright-slide"
+            >
+              {t("visit-live-version")}
+            </Link>
+
+            <Link
+              type="external"
+              href={project.githubUrl}
+              animation="bright-slide"
+            >
+              {t("visit-github")}
+            </Link>
+          </div>
+        )}
+
+        <ProjectInfosList project={project} />
       </section>
     </div>
   );
