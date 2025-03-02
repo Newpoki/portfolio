cb780331d8d802e8af64133fcd41710227ad5408
Jason Savelli <jsavelli@splio.com>
2025-03-02 14:21:30 +0100

    feat: routing pathnames adapt to locale

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -6,6 +6,25 @@ export const routing = defineRouting({
 
   // Used when no locale matches
   defaultLocale: "en",
+
+  pathnames: {
+    "/projects": {
+      en: "/projects",
+      fr: "/projets",
+    },
+    "/projects/[project-slug]": {
+      en: "/projects/[project-slug]",
+      fr: "/projets/[project-slug]",
+    },
+    "/experiencies": {
+      en: "/experiencies",
+      fr: "/experiences",
+    },
+    "/about": {
+      en: "/about",
+      fr: "/a-propos",
+    },
+  },
 });
 
 export type Locale = (typeof routing.locales)[number];
