b68bf16c94ddcc763b8ade6332f1296a11e8b0ff
Jason Savelli <savellijason@gmail.com>
2025-10-09 19:12:07 +0200

    fix: add missing options

--- app/[locale]/projects/[project-slug]/project-constants.ts
+++ app/[locale]/projects/[project-slug]/project-constants.ts
@@ -1,4 +1,4 @@
-import { Bundler, Framework } from "@prisma/client";
+import { Bundler, Framework, UserInterfaceLibrary } from "@prisma/client";
 
 export const FRAMEWORK_OPTIONS = {
   NEXT: { value: Framework.NEXT, label: "Next JS" },
@@ -10,4 +10,20 @@ export const BUNDLER_OPTIONS = {
   TURBOPACK: { value: Bundler.TURBOPACK, label: "Turbopack" },
   VITE: { value: Bundler.VITE, label: "Vite" },
   WEBPACK: { value: Bundler.WEBPACK, label: "Webpack" },
+} as const;
+
+export const USER_INTERFACE_OPTIONS = {
+  MATERIAL_UI: {
+    value: UserInterfaceLibrary.MATERIAL_UI,
+    label: "Material UI",
+  },
+  SHADCN: { value: UserInterfaceLibrary.SHADCN, label: "Shadcn" },
+  STYLED_COMPONENTS: {
+    value: UserInterfaceLibrary.STYLED_COMPONENTS,
+    label: "Styled Components",
+  },
+  TAILWIND_CSS: {
+    value: UserInterfaceLibrary.TAILWIND_CSS,
+    label: "Tailwind CSS",
+  },
 } as const;
--- app/[locale]/projects/[project-slug]/project-infos-list.tsx
+++ app/[locale]/projects/[project-slug]/project-infos-list.tsx
@@ -1,7 +1,11 @@
 import { TabeListItem } from "@/components/ui/table-list-item";
 import { Project } from "@prisma/client";
 import { getTranslations } from "next-intl/server";
-import { BUNDLER_OPTIONS, FRAMEWORK_OPTIONS } from "./project-constants";
+import {
+  BUNDLER_OPTIONS,
+  FRAMEWORK_OPTIONS,
+  USER_INTERFACE_OPTIONS,
+} from "./project-constants";
 
 type Props = {
   project: Project;
@@ -10,7 +14,7 @@ type Props = {
 export const ProjectInfosList = async ({ project }: Props) => {
   const t = await getTranslations("PROJECT");
 
-  const { bundler, framework } = project;
+  const { bundler, framework, userInterface } = project;
 
   const deployedAt = project.deployedAt.getFullYear();
 
@@ -19,11 +23,17 @@ export const ProjectInfosList = async ({ project }: Props) => {
       <TabeListItem label={t("deployed-at-title")}>
         {t("deployed-at-content", { deployedAt })}
       </TabeListItem>
+
       <TabeListItem label={t("bundler")}>
         {BUNDLER_OPTIONS[bundler].label}
       </TabeListItem>
+
       <TabeListItem label={t("framework")}>
         {FRAMEWORK_OPTIONS[framework].label}
+      </TabeListItem>
+
+      <TabeListItem label={t("user-interface")}>
+        {USER_INTERFACE_OPTIONS[userInterface].label}
       </TabeListItem>
 
       {/* TODO: Add back data fetching */}
