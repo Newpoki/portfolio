1f52d720261b8f3e0b188a0c40ec5fc336e8f471
Jason Savelli <savellijason@gmail.com>
2025-10-09 08:16:20 +0200

    task: remove useless todo

--- app/[locale]/experiencies/experiencies-actions.ts
+++ app/[locale]/experiencies/experiencies-actions.ts
@@ -6,10 +6,6 @@ const prisma = new PrismaClient();
 
 export const fetchExperiencies = async () => {
   const experiencies = await prisma.experience.findMany({
-    where: {
-      // TODO: Delete me, temp while working on admin panel
-      hidden: { isSet: false },
-    },
     orderBy: {
       startedAt: "desc",
     },
